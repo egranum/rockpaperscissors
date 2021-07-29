a021c4bc4a47a8977baf7021457bba189475e572
egranum <elliot.granum@ways.no>
2021-07-29 11:09:05 +0200

    Make game loop, improve result

--- script.js
+++ script.js
@@ -19,132 +19,50 @@ let result;
 function playRound(playerSelection, computerSelection) {
 
     if (playerSelection == "rock" && computerSelection === "scissors") {
-        result = "You win! Rock beats Scissors";
+        result = 1;
     } else if (playerSelection == "paper" && computerSelection === "rock") {
-        result = "You win! Paper beats Rock";
+        result = 1;
     } else if (playerSelection == "scissors" && computerSelection === "paper") {
-        result = "You win! Scissors beat Paper";
+        result = 1;
     } else if (playerSelection == "rock" && computerSelection === "paper") {
-        result = "You lose! Paper beats Rock";
+        result = -1;
     } else if (playerSelection == "paper" && computerSelection === "scissors") {
-        result = "You lose! Scissors beat Paper";
+        result = -1;
     } else if (playerSelection == "scissors" && computerSelection === "rock") {
-        result = "You lose! Rock beats Scissors";
+        result = -1;
     } else if (playerSelection == computerSelection) {
-        result = "It's a draw!";
+        result = 0;
     } else {
         result = "You need to choose a weapon!";
     }
 
     console.log("Your choice: " + playerSelection);
     console.log("Computer's choice: " + computerSelection);
-    console.log(result);
 }
 
 function game() {
     let x = 0;
     let y = 0;
-    personPlay();
-    computerPlay();
-    playRound(playerSelection, computerSelection);
+    for (let i = 0; i < 5; i++) {
+        personPlay();
+        computerPlay();
+        playRound(playerSelection, computerSelection);
 
-    switch (result) {
-        case "You win! Rock beats Scissors":
-        case "You win! Paper beats Rock":
-        case "You win! Scissors beat Paper":
-            x++;
+        switch(result) {
+            case 1: x++;
             break;
-        case "You lose! Paper beats Rock":
-        case "You lose! Scissors beat Paper":
-        case "You lose! Rock beats Scissors":
-            y++;
+            case -1: y++;
             break;
+        }
     }
 
-
-    personPlay();
-    computerPlay();
-    playRound(playerSelection, computerSelection);
-    
-    switch (result) {
-        case "You win! Rock beats Scissors":
-        case "You win! Paper beats Rock":
-        case "You win! Scissors beat Paper":
-            x++;
-            break;
-        case "You lose! Paper beats Rock":
-        case "You lose! Scissors beat Paper":
-        case "You lose! Rock beats Scissors":
-            y++;
-            break;
-    }
-
-
-    personPlay();
-    computerPlay();
-    playRound(playerSelection, computerSelection);
-
-    switch (result) {
-        case "You win! Rock beats Scissors":
-        case "You win! Paper beats Rock":
-        case "You win! Scissors beat Paper":
-            x++;
-            break;
-        case "You lose! Paper beats Rock":
-        case "You lose! Scissors beat Paper":
-        case "You lose! Rock beats Scissors":
-            y++;
-            break;
-    }
-
-
-    personPlay();
-    computerPlay();
-    playRound(playerSelection, computerSelection);
-
-    switch (result) {
-        case "You win! Rock beats Scissors":
-        case "You win! Paper beats Rock":
-        case "You win! Scissors beat Paper":
-            x++;
-            break;
-        case "You lose! Paper beats Rock":
-        case "You lose! Scissors beat Paper":
-        case "You lose! Rock beats Scissors":
-            y++;
-            break;
-    }
-
-
-    personPlay();
-    computerPlay();
-    playRound(playerSelection, computerSelection);
-
-    switch (result) {
-        case "You win! Rock beats Scissors":
-        case "You win! Paper beats Rock":
-        case "You win! Scissors beat Paper":
-            x++;
-            break;
-        case "You lose! Paper beats Rock":
-        case "You lose! Scissors beat Paper":
-        case "You lose! Rock beats Scissors":
-            y++;
-            break;
-    }
-
-
-    let score;
-
-    if (x > y) {
-        score = "Congrats! You won " + x + " out of 5 games";
-    } else if (x < y) {
-        score = "You lost! Computer won " + y + " games out of 5. Better luck next time";
+    if (x < y) {
+        console.log("You lose");
+    } else if (x > y) {
+        console.log("You win!");
     } else {
-        score = "It's a tie, try again!";
+        console.log("It's a tie");
     }
-
-    console.log(score);
 }
 
 game();
