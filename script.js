42a85b6c8d257db180a9eb09ac8b7840f8767c1a
egranum <elliot.granum@ways.no>
2021-08-02 13:06:07 +0200

    Get player's choice from buttons

--- script.js
+++ script.js
@@ -7,62 +7,89 @@ function computerPlay() {
     computerSelection = weapon[random];
 }
 
-let playerSelection;
+// Old one, might be deleted
+/*
+function getPlayerSelection() {
+    while (true) {
+        const playerInput = prompt("Choose your weapon: Rock, paper or scissors!", '').toLowerCase();
+        if (weapon.includes(playerInput)) {
+            return playerInput
+        }
+        console.log("You need to choose a weapon!")
+    }
+}
+*/
+function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
 
-function personPlay() {
-    playerSelection = prompt("Choose your weapon: Rock, paper or scissors!", '').toLowerCase();
+const winTable = {
+    rock: "scissors",
+    paper: "rock",
+    scissors: "paper"
 }
 
-
-let result;
+function findWinner(first, second) {
+    if (winTable[first] === second) {
+        console.log(`${capitalizeFirstLetter(first)} beats ${second}`)
+        return 1
+    } 
+    if (winTable[second] === first) {
+        console.log(`${capitalizeFirstLetter(second)} beats ${first}`)
+        return -1
+    }
+    console.log(`${capitalizeFirstLetter(first)} ties ${second}`)
+    return 0
+}
 
 function playRound(playerSelection, computerSelection) {
 
-    if (playerSelection == "rock" && computerSelection === "scissors") {
-        result = 1;
-    } else if (playerSelection == "paper" && computerSelection === "rock") {
-        result = 1;
-    } else if (playerSelection == "scissors" && computerSelection === "paper") {
-        result = 1;
-    } else if (playerSelection == "rock" && computerSelection === "paper") {
-        result = -1;
-    } else if (playerSelection == "paper" && computerSelection === "scissors") {
-        result = -1;
-    } else if (playerSelection == "scissors" && computerSelection === "rock") {
-        result = -1;
-    } else if (playerSelection == computerSelection) {
-        result = 0;
-    } else {
-        result = "You need to choose a weapon!";
+    if (!winTable?.[playerSelection]) {
+        return null
     }
-
     console.log("Your choice: " + playerSelection);
     console.log("Computer's choice: " + computerSelection);
+    const winner = findWinner(playerSelection ,computerSelection)
+    
+    return winner
 }
 
+const buttons = document.querySelector('.buttons')
+
+buttons.addEventListener('click', function(e) {
+    const playerSelection = e.target.className.toString();
+    computerPlay();
+    playRound(playerSelection, computerSelection);
+})
+
+/*
 function game() {
-    let x = 0;
-    let y = 0;
+    let playerScore = 0;
+    let computerScore = 0;
     for (let i = 0; i < 5; i++) {
-        personPlay();
+        const playerSelection = getPlayerSelection();
         computerPlay();
-        playRound(playerSelection, computerSelection);
+        const result = playRound(playerSelection, computerSelection);
 
         switch(result) {
-            case 1: x++;
-            break;
-            case -1: y++;
-            break;
+            case 1:
+                playerScore++;
+                break;
+            case -1: 
+                computerScore++;
+                break;
+            case null:
+                console.log("You need to choose a weapon!")
+                break;
         }
     }
 
-    if (x < y) {
+    if (playerScore < computerScore) {
         console.log("You lose");
-    } else if (x > y) {
+    } else if (playerScore > computerScore) {
         console.log("You win!");
     } else {
         console.log("It's a tie");
     }
 }
-
-game();
+*/
